bb778fc176f404f40dee69b934670167a83377ed
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2021-03-12 17:50:07 +1100

    adding a different algo .. looking to improve performance

--- nba_database_version2/dbo/Tables/Algo.sql
+++ nba_database_version2/dbo/Tables/Algo.sql
@@ -7,12 +7,10 @@
 --group by player.tm, Player.Season
 --order by Player.Season ASC, totalPlayers desc
 
-select Player.Season, Player.tm, COUNT(*) as totalPlayers 
+select Player.Season, Player.tm, sum(PER) as per_rating, sum(MP) as mins_played ,sum(TS) as true_shooting 
+, sum(TOV) as Turnovers, sum (TRB) as total_rebounds
 from Player
-where (PER Between 12.9 AND 129)
-
-AND (G Between 50 AND 82)
-AND (MP Between 900 AND 3167)
+where (G Between 50 AND 82)
 
 group by player.tm, Player.Season
-order by Player.Season ASC, totalPlayers desc
+order by Player.Season ASC, per_rating desc, mins_played desc, true_shooting desc, Turnovers ASC, total_rebounds desc
