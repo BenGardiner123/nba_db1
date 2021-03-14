817a3e840688233fbc73c94f4490b10ca3e9d9e6
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2021-03-15 08:43:19 +1100

    should be better

--- nba_database_version2/dbo/Tables/Algo.sql
+++ nba_database_version2/dbo/Tables/Algo.sql
@@ -7,10 +7,10 @@
 --group by player.tm, Player.Season
 --order by Player.Season ASC, totalPlayers desc
 
-select Player.Season, Player.tm, sum(PER) as per_rating, sum(MP) as mins_played ,sum(TS) as true_shooting 
-, sum(TOV) as Turnovers, sum (TRB) as total_rebounds
-from Player
-where (G Between 50 AND 82)
+--select Player.Season, Player.tm, sum(PER) as per_rating, sum(MP) as mins_played ,sum(TS) as true_shooting 
+--, sum(TOV) as Turnovers, sum (TRB) as total_rebounds
+--from Player
+--where (G Between 50 AND 82)
 
-group by player.tm, Player.Season
-order by Player.Season ASC, per_rating desc, mins_played desc, true_shooting desc, Turnovers ASC, total_rebounds desc
+--group by player.tm, Player.Season
+--order by Player.Season ASC, per_rating desc, mins_played desc, true_shooting desc, Turnovers ASC, total_rebounds desc
