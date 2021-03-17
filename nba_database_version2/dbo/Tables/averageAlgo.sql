85132645c52d096e1c170d5214742c6d01891ca7
Ben Gardiner <bennygards@gmail.com>
2021-03-17 21:03:39 +1100

    2

--- nba_database_version2/dbo/Tables/averageAlgo.sql
+++ nba_database_version2/dbo/Tables/averageAlgo.sql
@@ -1,21 +1,7 @@
---Select player_w_wins.Season, 
---player_w_wins.TEAM_ABBREVIATION, player_w_wins.PLAYER_ID,
---SUM(player_w_wins.PTS) as points, 
---SUM(player_w_wins.MINS) as mins_played,
---sum(player_w_wins.FG_PCT) as fgpct, 
---sum(player_w_wins.PLUS_MINUS) as plusMinus, 
---sum(player_w_wins.GP) as gamesPlayed, 
-
-
---SUM(PLUS_MINUS * PTS / (MINS/GP) ) as weirdAlgo
---from player_w_wins
-
---group by player_w_wins.Season, player_w_wins.TEAM_ABBREVIATION
---order by player_w_wins.Season asc, weirdAlgo desc
-
-Select player_w_wins.Season, 
-player_w_wins.TEAM_ABBREVIATION, PLAYER_NAME ,player_w_wins.PLAYER_ID, Sum(player_w_wins.PLUS_MINUS) as plusMin, 
-player_w_wins.PTS as PTS, player_w_wins.MINS as minZ, player_w_wins.GP as gamesplayed, SUM(plusMin * points) as calc
-
+select player_w_wins.PLAYER_NAME, player_w_wins.PLAYER_ID, player_w_wins.TEAM_ABBREVIATION, player_w_wins.Season, player_w_wins.GP,
+AVG(PLUS_MINUS) as plusMin, 
+AVG(PTS) as PTS, 
+AVG(MINS) as minZ
 from player_w_wins
-where Season = 201112 and season = 201213;
+where player_w_wins.Season 
+group by player_w_wins.Season, TEAM_ABBREVIATION, PLAYER_ID, PLAYER_NAME, GP
