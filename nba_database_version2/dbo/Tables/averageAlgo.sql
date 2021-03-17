62c18df17a013d39d6418f56dd51d9afc253bf21
Ben Gardiner <bennygards@gmail.com>
2021-03-17 19:50:53 +1100

    working on avg

--- nba_database_version2/dbo/Tables/averageAlgo.sql
+++ nba_database_version2/dbo/Tables/averageAlgo.sql
@@ -14,12 +14,8 @@
 --order by player_w_wins.Season asc, weirdAlgo desc
 
 Select player_w_wins.Season, 
-player_w_wins.TEAM_ABBREVIATION, PLAYER_NAME ,player_w_wins.PLAYER_ID, player_w_wins.PLUS_MINUS as plusMin, 
-player_w_wins.PTS as PTS, player_w_wins.MINS as minZ, player_w_wins.GP as gamesplayed, 
+player_w_wins.TEAM_ABBREVIATION, PLAYER_NAME ,player_w_wins.PLAYER_ID, Sum(player_w_wins.PLUS_MINUS) as plusMin, 
+player_w_wins.PTS as PTS, player_w_wins.MINS as minZ, player_w_wins.GP as gamesplayed, SUM(plusMin * points) as calc
 
-avg(player_w_wins.PLUS_MINUS) over(partition by player_ID) as careerPlusMinusAvg,
-AVG(player_w_wins.PTS) over(partition by player_ID) as careerPtsAvg,
-AVG(player_w_wins.MINS) over(partition by player_ID) as careerMinsAvg,
-AVG(player_w_wins.GP) over(partition by player_ID) as careerPtsAvg
 from player_w_wins
-where Season between 201112 and 201516
+where Season = 201112 and season = 201213;
